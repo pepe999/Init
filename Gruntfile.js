80c91161cb2e785491c15919d40273e8ddba6266
pepe999 <mail@josefbina.cz>
2016-03-15 21:02:44 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,18 @@
 module.exports = function (grunt) { // obalovací funkce 
     grunt.initConfig({ 
         pkg: grunt.file.readJSON('package.json'), // cesta ke konfiguračnímu package.json 
+        
+        uglify: {
+            default: {
+                files: {
+                'parts/js/script.min.js': [
+                    'src/js/A.js',
+                    'src/js/B.js'
+                ]
+                }
+            }
+        },
+    
         // kompilace LESS souboru 
         less: { // název úlohy a její nastavení 
             dev: { 
@@ -8,11 +20,13 @@ module.exports = function (grunt) { // obalovací funkce
                     "parts/css/style.css": ["src/less/index.less" ] 
                 } 
             } 
-        } 
+        }
+         
     }); 
   
     grunt.loadNpmTasks('grunt-contrib-less'); // chci načíst balíček grunt-contrib-less 
+    grunt.loadNpmTasks('grunt-contrib-uglify');
   
-    grunt.registerTask('default', ['less']); // registrace defaultní úlohy 
+    grunt.registerTask('default', ['less','uglify']); // registrace defaultní úlohy 
     
  }; 
